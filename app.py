cbbe7872269e851c067dc14cb0745814a46615c3
M.ALI SALEEM <1semasterx@gmail.com>
2026-01-08 02:49:16 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+
+
+face_dec = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+
+camera = cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = face_dec.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30,30)
+        )
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+
+        
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
